ccafffb3ad86cf029209173db7a3991c39a841a4
TC5 <thechua1@gmail.com>
2019-01-01 12:18:08 +0700

    second project

---
+++ personel/views.py
@@ -0,0 +1,8 @@
+﻿from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'personel/home.html')
+
+def contact(request):
+    return render(request, 'personel/basic.html', {'content':['Line:@veeclassy (มี@นะคะ) ','IG : veeclassy314']})
---
+++ webapp/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('webapp/', views.index, name='index')]
